4cd818eae4e357f1058d1be57b51ee5a1ea49d3e
Paramveer <68002445+paramveer-cyber@users.noreply.github.com>
2026-06-17 17:23:51 +0530

    second final commit

--- backend/node_modules/inngest/helpers/types.cjs
+++
@@ -1,9 +0,0 @@
-
-//#region src/helpers/types.ts
-function isRecord(value) {
-	return typeof value === "object" && value !== null && !Array.isArray(value);
-}
-
-//#endregion
-exports.isRecord = isRecord;
-//# sourceMappingURL=types.cjs.map
--- backend/node_modules/inngest/helpers/types.js
+++
@@ -1,8 +0,0 @@
-//#region src/helpers/types.ts
-function isRecord(value) {
-	return typeof value === "object" && value !== null && !Array.isArray(value);
-}
-
-//#endregion
-export { isRecord };
-//# sourceMappingURL=types.js.map
